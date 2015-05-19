ffd649f9d59123411f4df10542711a81bc32803b
Jackie Johnston <jackiejohnston@users.noreply.github.com>
2015-05-19 14:56:35 -0700

    Create mock-data.js

---
+++ mock-data.js
@@ -0,0 +1,16 @@
+JSON_CALLBACK(
+  {
+    "LoanNumber": "7000310777",
+    "LoanProgram": "VA IRRRL 30 Fixed",
+    "LoanAmount": 139440.0000,
+    "LoanPurpose": "NoCash-Out Refinance",
+    "Term": 360,
+    "InterestRate": 4.25000,
+    "Points": 0.0000000000,
+    "APR": 4.25000,
+    "MonthlyPayment": 970.1200000000,
+    "LockStatus": "NotLocked",
+    "Expirationdate": "0001-01-01T00:00:00",
+    "EstimatedClosingDate": "2014-09-23T00:00:00"
+  }
+);
